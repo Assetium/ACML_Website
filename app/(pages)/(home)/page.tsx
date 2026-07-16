338dfc694623cbbdcd0c5bd9e122e879a700e8f5
KPAMsar <kpamsarshija@gmail.com>
2026-07-16 17:21:31 +0100

    fix

--- app/(pages)/(home)/page.tsx
+++ app/(pages)/(home)/page.tsx
@@ -1,55 +1,50 @@
-// import Hero from "@/app/sections/hero";
-// import Metrics from "@/app/sections/metrics";
-// // import ImageSlider from "@/app/sections/image-slider";
-// // import HomeAbout from "@/app/sections/home-about";
-// import HomeInsights from "@/app/sections/home-insights";
-// // import OpenAccountCTA from "@/app/sections/open-account-cta";
-// import Testimonials from "@/app/sections/home-testimonials";
-// // import backgroundImage from "@/public/home/hero-bg.png";
-// import GCEOMessage from "@/app/sections/gceo-message";
-// import OurBusinesses from "@/app/sections/our-businesses";
 
-// export default function Home() {
+
+// import React from "react";
+// import AIMLHeader from "@/app/sections/aiml-header";
+// import AIMLServices from "@/app/sections/aiml-service";
+// import AIMLProducts from "@/app/sections/aiml-products";
+// import AIMLInsights from "@/app/sections/aiml-insighta";
+// import AIMWhyUs from "@/app/sections/aiml-why-choose-us";
+
+// export default function ComingSoon() {
 //   return (
-//     <div>
-      
-//       <div className="h-full py-9 relative z-10">
-//         <Hero />
-//       </div>
-//       <div className="relative w-full">
-//         <div className=" w-full absolute left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10">
-//           <Metrics />
-//         </div>
-
-//         {/* <HomeAbout /> */}
-//       </div>
-//       {/* <ImageSlider /> */}
-//       <OurBusinesses />
-//       {/* <OpenAccountCTA /> */}
-//       <GCEOMessage />
-//       <HomeInsights />
-//       <Testimonials />
-//     </div>
+//     <>
+//       <AIMLHeader />
+//       <AIMLServices />
+//       <AIMLProducts />
+//       <AIMLInsights />
+//       <AIMWhyUs />
+//     </>
+  
 //   );
 // }
 
 
+import ACMLHeader from "@/app/sections/acml-header";
+import ACMLManagement from "@/app/sections/acml-management";
+import ACMLProuctOffernings from "@/app/sections/acml-product-offernings";
+import ACMLServiceOfferings from "@/app/sections/acml-service-offernings";
+import ATCCommoditiesCycles from "@/app/sections/atcl-commodity-cycles";
+import ATCTrancheHistory from "@/app/sections/atcl-tranche-history";
+import ATCHowItWorks from "@/app/sections/how-actl-work";
+import OtherProducts from "@/app/sections/other-acml-products";
 import React from "react";
-import AIMLHeader from "@/app/sections/aiml-header";
-import AIMLServices from "@/app/sections/aiml-service";
-import AIMLProducts from "@/app/sections/aiml-products";
-import AIMLInsights from "@/app/sections/aiml-insighta";
-import AIMWhyUs from "@/app/sections/aiml-why-choose-us";
 
-export default function ComingSoon() {
+const Acml = () => {
   return (
-    <>
-      <AIMLHeader />
-      <AIMLServices />
-      <AIMLProducts />
-      <AIMLInsights />
-      <AIMWhyUs />
-    </>
-  
+    <div>
+      <ACMLHeader />
+      <ACMLServiceOfferings />
+      <ACMLProuctOffernings />
+      <ATCCommoditiesCycles />
+      <ATCHowItWorks />
+      <ATCTrancheHistory />
+      <OtherProducts />
+
+      <ACMLManagement />
+    </div>
   );
-}
+};
+
+export default Acml;
